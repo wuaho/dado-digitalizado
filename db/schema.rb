7adbcb7552069dea0eac745d18cfef281beafe68
wuaho <juanjoserequenalama@gmail.com>
2022-03-02 17:22:50 +0100

    add nickname to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_02_092944) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_02_155641) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_02_092944) do
     t.date "birthdate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
   end
 
 end
