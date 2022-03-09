80606c85f7d48beee75b3dad39860715f388888a
wuaho <juanjoserequenalama@gmail.com>
2022-03-09 17:47:34 +0100

    Add routes for admin and respective controllers-views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
   devise_for :users
+
+  namespace :admin do
+    # The controllers that will be called are the ones in app/controllers/admin directory
+    resources :users
+  end
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'home#index'
 end
