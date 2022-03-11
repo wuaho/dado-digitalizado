58b0738690f63f53e2df6bc43104788372616fa1
wuaho <juanjoserequenalama@gmail.com>
2022-03-11 14:43:25 +0100

    Remove active_storage routes

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,21 @@
 require_relative "boot"
 
-require "rails/all"
+# This line adds too many libraries that Im not really using, below are the requires 1 by 1 that 
+# I really need. Check out what rails/all.rb is currently expanded to:
+# https://github.com/rails/rails/blob/master/railties/lib/rails/all.rb
+
+# require "rails/all"
+require 'active_record/railtie'
+# require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'active_job/railtie'
+require 'action_cable/engine'
+# require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,6 +27,8 @@ module Dado
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
+    # This line removes active_storage generated routes
+    config.active_storage.draw_routes = false
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
