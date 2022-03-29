05a1be56a0dd88d5d91649b87a84b6e54cb4fca5
wuaho <juanjoserequenalama@gmail.com>
2022-03-29 18:20:54 +0200

    Add unfinished scope for borrowing

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -1,4 +1,7 @@
 class Borrowing < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  #This doesnt work properly now, the starts on at least is not
+  scope :active, -> { where('starts_on <= ? <= expires_on', Date.today) }
 end
