db3075ecc538d6770e0232846eccbe2ce52c4bdc
wuaho <juanjoserequenalama@gmail.com>
2022-03-30 18:35:42 +0200

    Added borrowing controller and views to manage them

---
+++ app/controllers/borrowings_controller.rb
@@ -0,0 +1,40 @@
+class BorrowingsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_borrowing, only: %i[show]
+
+  def index
+    @borrowings = current_user.borrowings
+  end
+
+  def show; end
+
+  def new
+    @game_id = params[:game_id]
+    @borrowing = Borrowing.new(game_id: @game_id, user_id: current_user.id, starts_on: Date.today,
+                               expires_on: Date.today + 7)
+  end
+
+  def create
+    @borrowing = Borrowing.new(borrowing_params)
+
+    respond_to do |format|
+      if @borrowing.save
+        format.html { redirect_to borrowing_url(@borrowing), notice: 'Borrowing was successfully created.' }
+        format.json { render :show, status: :created, location: @borrowing }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @borrowing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_borrowing
+    @borrowing = Borrowing.find(params[:id])
+  end
+
+  def borrowing_params
+    params.require(:borrowing).permit(:starts_on, :expires_on, :game_id, :user_id)
+  end
+end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   before_action :set_game, only: %i[show]
-
+  before_action :set_user, only: %i[show]
   # GET /games or /games.json
   def index
     @games = Game.all
@@ -14,5 +14,9 @@ class GamesController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_game
     @game = Game.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
   end
 end
