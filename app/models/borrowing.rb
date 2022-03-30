a42525ef5277caab56c0b436ae9085a6ad5156b7
wuaho <juanjoserequenalama@gmail.com>
2022-03-30 10:47:54 +0200

    Fix active Borrowings scope

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -2,6 +2,5 @@ class Borrowing < ApplicationRecord
   belongs_to :user
   belongs_to :game
 
-  #This doesnt work properly now, the starts on at least is not
-  scope :active, -> { where('starts_on <= ? <= expires_on', Date.today) }
+  scope :active, -> { where('starts_on <= ?', Date.today).where('expires_on >= ?', Date.today) }
 end
