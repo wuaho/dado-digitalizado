c242db037c17aba89e6d93e2c4321d80a82a36aa
wuaho <juanjoserequenalama@gmail.com>
2022-03-30 10:51:04 +0200

    Make expiration date of mock borrowing to end later

--- spec/factories/borrowings.rb
+++ spec/factories/borrowings.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :borrowing do
     starts_on { "2022-03-28" }
-    expires_on { "2022-03-30" }
+    expires_on { "2022-10-30" }
     association :user
     association :game
   end
