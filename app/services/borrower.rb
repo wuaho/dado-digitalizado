3612f06ba58fb12bc0b82777885e8bb97bbc90f9
wuaho <juanjoserequenalama@gmail.com>
2022-03-30 10:52:03 +0200

    Remove useless comments

--- app/services/borrower.rb
+++ app/services/borrower.rb
@@ -7,11 +7,6 @@ class Borrower
   end
 
   def user_can_borrow_game?
-    # Checks that:
-    #   1. User is not banned
-    #   2. User has member role or higher
-    #   3. User has no active borrowing
-    #   4. Game stock is bigger than number of active borrowings of that game
     user_not_banned? && user_is_member? && user_has_no_active_borrowing? && game_is_available?
   end
 
