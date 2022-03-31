1fb87a09d6eaa043d7be7b41d948de5f83c0def8
wuaho <juanjoserequenalama@gmail.com>
2022-03-31 09:21:00 +0200

    Remove set_user from games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,22 +1,15 @@
 class GamesController < ApplicationController
   before_action :set_game, only: %i[show]
-  before_action :set_user, only: %i[show]
-  # GET /games or /games.json
+
   def index
     @games = Game.all
   end
 
-  # GET /games/1 or /games/1.json
   def show; end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_game
     @game = Game.find(params[:id])
-  end
-
-  def set_user
-    @user = current_user
   end
 end
