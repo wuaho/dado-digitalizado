65a769f392c175513e102b448dbaa65d7ba1cfef
wuaho <juanjoserequenalama@gmail.com>
2022-03-31 16:03:24 +0200

    Ensure user can borrow game before controller methods

--- app/controllers/borrowings_controller.rb
+++ app/controllers/borrowings_controller.rb
@@ -10,14 +10,15 @@ class BorrowingsController < ApplicationController
 
   def new
     @game_id = params[:game_id]
+    validate_borrow!(@game_id)
     @borrowing = Borrowing.new(game_id: @game_id)
   end
 
   def create
-    # TO-DO user id is automatically introduced in the next line as current_user.id and it doesnt come
-    # from the form
-    @borrowing = Borrowing.new(borrowing_params.merge(user_id: current_user.id))
+    @game_id = params.dig(:borrowing, :game_id)
+    validate_borrow!(@game_id)
 
+    @borrowing = Borrowing.new(borrowing_params.merge(user_id: current_user.id))
     respond_to do |format|
       if @borrowing.save
         format.html { redirect_to borrowing_url(@borrowing), notice: 'Borrowing was successfully created.' }
@@ -31,11 +32,18 @@ class BorrowingsController < ApplicationController
 
   private
 
+  def validate_borrow!(game_id)
+    @game = Game.find(game_id)
+    return if Borrower.new(current_user, @game).user_can_borrow_game?
+
+    redirect_to(game_url(@game), alert: 'Forbidden')
+  end
+
   def set_borrowing
     @borrowing = Borrowing.find(params[:id])
   end
 
   def borrowing_params
-    params.require(:borrowing).permit(:starts_on, :expires_on,:game_id)
+    params.require(:borrowing).permit(:starts_on, :expires_on, :game_id)
   end
 end
