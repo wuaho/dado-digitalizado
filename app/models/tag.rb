cfe6ffd413e6b4cee0e7984515837f05718a8307
wuaho <juanjoserequenalama@gmail.com>
2022-04-05 09:54:44 +0200

    Add Tag model validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,8 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :games
+
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            length: { minimum: 3, too_short: '%{count} characters is the minimum allowed' }
 end
